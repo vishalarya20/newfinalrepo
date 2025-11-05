97c00c9e68185eae5f281f2a1bf87c4f78ce65e3
sapcoiot00 <sapcoiotpvtltd@gmail.com>
2025-11-05 10:12:04 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layouts/Layout";
 
 // Pages
@@ -7,28 +7,16 @@ import Login from "./pages/auth/Login";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
-	const isAuthenticated = true; // change to false to test
-
 	return (
 		<Router>
 			<Routes>
-				{/* AUTH PAGES — visible only when NOT logged in */}
-				<Route
-					path="/login"
-					element={
-						!isAuthenticated ? <Login /> : <Navigate to="/" replace />
-					}
-				/>
-				
+				{/* Without Layout */}
+				<Route path="/login" element={<Login />} />
 
-				{/* DASHBOARD PAGES — visible only when logged in */}
-				{isAuthenticated ? (
-					<Route element={<Layout />}>
-						<Route path="/" element={<AdminDashboard />} />
-					</Route>
-				) : (
-					<Route path="*" element={<Navigate to="/login" replace />} />
-				)}
+				{/* With Layout */}
+				<Route element={<Layout />}>
+					<Route path="/" element={<AdminDashboard />} />
+				</Route>
 			</Routes>
 		</Router>
 	);
